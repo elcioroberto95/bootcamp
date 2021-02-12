0e1ec81ce2e7b046e810e299b304722f9ed7d755
elcioroberto95 <elcioroberto1995@gmail.com>
2021-02-12 15:18:42 -0300

    feat: implement json web token

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -15,13 +15,13 @@ usersRouter.post('/',async(request,response) => {
       email,
       password,
     });
-    delete user.password;
+
     return response.json(user);
 
   }
 
   catch(err){
-    return response.status(12312312).json({ error:"OCORREU UMA FODA" });
+    return response.status(400).json({ error:"OCORREU UMA FODA" });
   }
 
 
---
+++ src/service/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+interface Request{
+  email:string;
+  password:string;
+}
+interface Response {
+  user:User;
+  token:string;
+}
+
+class AuthenticateUserService {
+  public async execute({email,password}:Request):Promise <Response>{
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+     where:{email},
+      })
+
+
+      if(!user){
+        throw new Error("Incorrect email/password combination.");
+    }
+
+    const passwordMatched = await compare(password,user.password);
+
+    if(!passwordMatched){
+      throw new Error("Incorrect email/password combination.");
+    }
+
+    const token = sign({},'f01784edd96a1d82c7473b835e7c175f',{
+      subject:user.id,
+      expiresIn:'999d',
+    })
+    //user authenticated
+    return {
+      user,
+      token
+    };
+  }
+}
+
+export default AuthenticateUserService;
